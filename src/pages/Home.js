796dc90313a242ca50d1c471470a3dbb8612917d
JenabB <birulangit1996@gmail.com>
2021-08-28 22:24:29 +0700

    animation on welcome page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,10 +4,15 @@ import FloatingAddButton from "../components/FloatingAddButton";
 // import Footer from "./home/Footer";
 import History from "./home/History";
 import TotalAlokasiDana from "./home/TotalAlokasiDana";
-
+import { motion } from "framer-motion";
 const Home = () => {
   return (
-    <div>
+    <motion.div
+      initial={{ scaleY: 0 }} //This is the style of the component at the beginning of the animation when it is animating in.
+      animate={{ scaleY: 1 }} //This is the style of the component at the end of the animation when it is animating in.
+      exit={{ scaleY: 0 }} //This is the style of the component at the end of the animation when it is animating out.
+      transition={{ duration: 0.5 }} //Transition configuration. Here, we're specifying how long we want the duration to last for (in our case, 0.5 seconds).
+    >
       <AppBar />
       <div className="lg:w-2/4 mx-auto w-full">
         <TotalAlokasiDana />
@@ -15,7 +20,7 @@ const Home = () => {
       </div>
       <FloatingAddButton />
       {/* <Footer /> */}
-    </div>
+    </motion.div>
   );
 };
 
--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import welcome from "../assets/images/welcome.jpg";
+import { motion } from "framer-motion";
+
 const Welcome = () => {
   return (
     <div>
@@ -8,20 +10,77 @@ const Welcome = () => {
         <img src={welcome} alt="welcome" />
       </div>
 
-      <div className="p-4 lg:w-1/4 mx-auto">
-        <h1 className="text-3xl text-center">Catat Pendanaan</h1>
-        <p className="mt-4">
+      <div className="p-4 pt-0 lg:w-1/4 mx-auto">
+        <motion.h1
+          className="text-3xl text-center"
+          initial="hidden"
+          animate="visible"
+          variants={{
+            hidden: {
+              scale: 0.8,
+              opacity: 0,
+            },
+            visible: {
+              scale: 1,
+              opacity: 1,
+              transition: {
+                delay: 0.9,
+              },
+            },
+          }}
+        >
+          Catat Pendanaan
+        </motion.h1>
+        <motion.p
+          className="mt-4"
+          initial="hidden"
+          animate="visible"
+          variants={{
+            hidden: {
+              scale: 0.8,
+              opacity: 0,
+            },
+            visible: {
+              scale: 1,
+              opacity: 1,
+              transition: {
+                delay: 1.4,
+              },
+            },
+          }}
+        >
           Catat alokasi pendanaanmu demi manajemen keuangan yang lebih baik.
-        </p>
-        <p className="text-sm mt-4">
+        </motion.p>
+        <motion.p
+          className="text-sm mt-4"
+          initial="hidden"
+          animate="visible"
+          variants={{
+            hidden: {
+              scale: 0.8,
+              opacity: 0,
+            },
+            visible: {
+              scale: 1,
+              opacity: 1,
+              transition: {
+                delay: 1.8,
+              },
+            },
+          }}
+        >
           Bukan seberapa banyak orang menghasilkan uang, melainkan untuk tujuan
           apa uang itu digunakan. <span>-John Ruskin</span>
-        </p>
+        </motion.p>
       </div>
       <Link to="/home" className="floating-button bottom-10 fixed">
-        <button className="bg-green-600 px-6 py-2 rounded-xl text-white">
+        <motion.div
+          animate={{ x: 100 }}
+          transition={{ ease: "easeOut", duration: 3 }}
+          className="bg-green-600 px-6 py-2 rounded-xl text-white"
+        >
           Mulai!
-        </button>
+        </motion.div>
       </Link>
     </div>
   );
